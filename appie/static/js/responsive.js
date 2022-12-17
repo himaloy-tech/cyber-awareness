edfca34eb8322bf7e20a5b8af89759174fd53bba
Himaloy Mondal <himaloymondal100@gmail.com>
2022-12-17 22:35:37 +0530

    files coming digital ocean spaces

--- appie/static/js/responsive.js
+++
@@ -1,8 +0,0 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('#navbar');
-nav_list = document.querySelector('#nav-list');
-
-burger.addEventListener('click', ()=>{
-    navbar.classList.toggle('h-nav');
-    nav_list.classList.toggle('v-nav');
-})
