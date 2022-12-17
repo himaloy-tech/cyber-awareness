e8ef5486873a7e96cf4f43db139c9ace0ef4b20c
Himaloy Mondal <himaloymondal100@gmail.com>
2022-12-17 11:56:25 +0530

    Added staticfiles serving through whitenoise

--- cyber/settings.py
+++ cyber/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,8 +127,8 @@ USE_TZ = True
 
 STATIC_ROOT=os.path.join(BASE_DIR, 'assets')
 STATIC_URL = 'appie/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "appie/static")
-]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
